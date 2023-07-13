422c7268add6b5b4ef289f86acd958d5be31c940
ankit kumar <ankit.kumar@stratos.studio.com>
2023-07-13 22:31:11 +0530

    changes

--- src/load_data.py
+++ src/load_data.py
@@ -12,8 +12,6 @@ def load_and_save(config_path):
     df.to_csv(raw_data_path, sep=",", index=False, header=new_cols)
 
 
-
-
 if __name__=="__main__":
     args = argparse.ArgumentParser()
     args.add_argument("--config", default="params.yaml")
